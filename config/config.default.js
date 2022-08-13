7e13b5b4ae039834444828531d6693533dd5b21d
Mallik Hiremath <Mallikarjunaiah.Hiremath@vanderbilt.edu>
2022-08-12 20:34:57 -0700

    Added the Dockerfile, docker-compose.yml, webgme.Dockerfile and .env file Updated the config.default.js to with docker as well as in standalone mode.

--- config/config.default.js
+++ config/config.default.js
@@ -4,9 +4,10 @@ var config = require('./config.webgme'),
     validateConfig = require('webgme/config/validator');
 
 // Add/overwrite any additional settings here
-//const mongoHost = process.env.MONGO_HOST || '127.0.0.1';
-//config.server.port = +process.env.PORT || 8080;
-//config.mongo.uri = `mongodb://mongo:27017`;
+// this will pick the appropriate MONGO_HOST value if being run on command then it would be 127.0.0.1
+// if being run as dockerized version then it will pick mongo as the db host name
+const mongoHost = process.env.MONGO_HOST || '127.0.0.1';
+config.mongo.uri = `mongodb://${mongoHost}:27017`;
 config.plugin.allowServerExecution = true;
 
 // Add/overwrite any additional settings here
@@ -14,5 +15,8 @@ config.plugin.allowServerExecution = true;
 // config.mongo.uri = 'mongodb://127.0.0.1:27017/webgme_my_app';
 config.requirejsPaths['jointjs'] = './node_modules/jointjs/dist/joint.min';
 config.requirejsPaths['lodash'] = './node_modules/lodash/lodash.min';
+
+config.authentication.enable = false;
+
 validateConfig(config);
 module.exports = config;
---
+++ webgme.Dockerfile
@@ -0,0 +1,28 @@
+# Node 14 on an Alpine
+FROM node:14
+
+RUN apt update
+RUN apt install -y software-properties-common sudo
+RUN add-apt-repository ppa:deadsnakes/ppa
+RUN apt install -y python3
+RUN apt install -y python3-pip
+RUN echo "alias python=python3" >> ~/.bashrc
+RUN echo "alias pip=pip3" >> ~/.bashrc
+
+# installing Python packages
+RUN pip3 install webgme-bindings
+RUN pip3 install jinja2
+RUN pip3 install networkx
+RUN pip3 install mako
+
+WORKDIR /usr/app
+ADD . /usr/app
+
+RUN npm install  --unsafe-perm
+RUN npm install -g npm@latest
+RUN npm install -g webgme-cli 
+# RUN npm install --save jointjs
+
+CMD ["npm", "start"]
+
+
